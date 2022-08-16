2aedad4ecc36037cda066c31436a038787333a66
Joshua Sprow <jsprow@gmail.com>
2022-08-16 11:44:05 -0400

    using tailwind styles on kvcc button

--- src/stories/KvccButton.stories.ts
+++ src/stories/KvccButton.stories.ts
@@ -1,18 +1,14 @@
 import { Meta, StoryFn } from "@storybook/vue3";
 import KvccButton from "./KvccButton.vue";
 
-// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 const meta: Meta<typeof KvccButton> = {
   title: "Example/KVCC Button",
   component: KvccButton,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
-    color: { type: { name: "string" } },
-    onClick: {},
-    size: {
-      control: { type: "select" },
-      options: ["sm", "md", "lg"],
+    color: {
+      type: { name: "string" },
     },
+    onClick: {},
     type: {
       type: { name: "enum", value: ["button", "submit", "reset"] },
     },
@@ -21,20 +17,20 @@ const meta: Meta<typeof KvccButton> = {
 
 export default meta;
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template: StoryFn<typeof KvccButton> = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
   components: { KvccButton },
-  // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args };
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
   template: '<kvcc-button v-bind="args" />',
 });
 
+export const Default = Template.bind({});
+Default.args = {
+  text: "Default",
+};
+
 export const Blue = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Blue.args = {
   color: "blue",
   text: "Blue",
@@ -47,7 +43,6 @@ Green.args = {
 };
 
 export const Red = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Red.args = {
   color: "red",
   text: "Red",
--- src/stories/KvccButton.vue
+++ src/stories/KvccButton.vue
@@ -3,34 +3,57 @@ import { computed } from "@vue/reactivity";
 import { withDefaults } from "vue";
 
 interface Props {
-  size?: string;
-  color: string;
+  color?: "blue" | "green" | "red";
   text: string;
   type?: "button" | "submit" | "reset";
 }
 
 const props = withDefaults(defineProps<Props>(), { type: "button" });
 
-const cssVars = computed(() => ({ "--button-color": props.color }));
+const colors = computed(() => {
+  switch (props.color) {
+    case "blue":
+      return ["#1d4ed8", "#1e40af"];
+    case "green":
+      return ["#047857", "#065f46"];
+    case "red":
+      return ["#b91c1c", "#991b1b"];
+    default:
+      return ["#334155", "#1e293b"];
+  }
+});
+
+const style = computed(() => ({
+  "--bg-color": colors.value[0],
+  "--bg-color-hover": colors.value[1],
+}));
 </script>
 
 <template>
-  <button :type="type" :class="color" :style="cssVars">
+  <button :type="type" :class="color" :style="style">
     <span>{{ text }}</span>
   </button>
 </template>
 
 <style>
-:root {
-  --font-size: 2rem;
-  --shadow-color: #444;
-  --shadow-size: calc(var(--font-size) * 0.25);
+button {
+  color: white;
+  background-color: var(--bg-color);
+  transition-property: all;
+  transition-duration: 300ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  font-weight: 500;
+  line-height: 1;
+  border: 1px solid var(--bg-color-hover);
+  border-radius: 0.25rem;
+  cursor: pointer;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  padding: 0.75rem 1rem;
 }
 
-button {
-  background-color: var(--button-color);
-  font-size: var(--font-size);
-  color: white;
-  text-shadow: 0 0 var(--shadow-size) var(--shadow-color);
+button:hover,
+button:active {
+  background-color: var(--bg-color-hover);
+  box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.5);
 }
 </style>
