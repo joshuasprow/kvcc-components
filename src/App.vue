4760997550dccf88384eb53e9e1a77c73e3f172f
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-20 17:49:09 -0400

    simplifying app and importing form page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import KvccButton from "./components/KvccButton.vue";
+import KvccFormPage from "./stories/KvccFormPage.vue";
 </script>
 
 <template>
-  <p>This is a button</p>
-  <KvccButton />
+  <KvccFormPage />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
