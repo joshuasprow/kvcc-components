e11b05807bc78fb72366eb55398012f4bd3f7e78
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2022-08-20 22:45:45 -0400

    turning typescript section back on

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -13,24 +13,22 @@ const config = {
   features: {
     storyStoreV7: true,
   },
-  // typescript: {
-  //   check: false,
-  //   checkOptions: {},
+  typescript: {
+    check: false,
+    checkOptions: {},
 
-  //   reactDocgen: "react-docgen-typescript",
-  //   reactDocgenTypescriptOptions: {
-  //     shouldExtractLiteralValuesFromEnum: true,
-  //     propFilter: (prop) =>
-  //       prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-  //   },
-  // },
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
   viteFinal: (config, { configType }) => {
-    console.log(JSON.stringify({ config }));
-    console.log(JSON.stringify({ configType }));
-    // if (configType === "PRODUCTION") {
-    //   // fixes absolute path to iframe.html, allowing successful deployment to github pages
-    //   config.base = "./";
-    // }
+    if (configType === "PRODUCTION") {
+      // fixes absolute path to iframe.html, allowing successful deployment to github pages
+      config.base = "./";
+    }
 
     return config;
   },
